17bb52de68a6246b5fc57c32abee38e7b968b458
unknown <randygoitia7@gmail.com>
2024-05-13 15:40:48 -0400

    agregando cambios al footer

--- iijp-app/src/Components/Footer.jsx
+++ iijp-app/src/Components/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "../Styles/footer.css";
+import { FaFacebook } from "react-icons/fa6";
+import { FaWhatsapp  } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { IoMail } from "react-icons/io5";
 const Footer = () => {
   return (
     <footer>
       <div className="footer flex flex-col">
-        <div>iconos</div>
+        <div id="footer-icons" className="flex flex-row  justify-center p-4"> <FaFacebook></FaFacebook> <FaWhatsapp></FaWhatsapp> <FaInstagram></FaInstagram> <FaPhoneAlt></FaPhoneAlt> <IoMail></IoMail></div>
         <div><p>© 2024 IIJP - Instituto de Investigaciones Juridicas y Politicas</p></div>
       </div>
     </footer>
