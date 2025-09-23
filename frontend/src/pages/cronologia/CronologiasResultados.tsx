9150f636a631b9dc846a6822a4136cd4529ede28
Randy <randygoitia7@gmail.com>
2025-09-23 15:35:08 -0400

    agregando aviso al salir del visor de documentos

--- frontend/src/pages/cronologia/CronologiasResultados.tsx
+++ frontend/src/pages/cronologia/CronologiasResultados.tsx
@@ -28,8 +28,27 @@ const CronologiasResultados = () => {
     return () => {
       if (pdfUrl) URL.revokeObjectURL(pdfUrl);
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pdfBlob]);
+
+  useEffect(() => {
+    const handleBeforeUnload = (event: { preventDefault: () => void; returnValue: string; }) => {
+      // Customize your message or logic here
+      const message =
+        "Estás a punto de salir de la página. Los cambios no guardados se perderán.";
+      event.preventDefault(); // Standard for browser compatibility
+      event.returnValue = message; // For older browsers
+      return message; // For newer browsers
+    };
+
+    // Add the event listener when the component mounts
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, []);
 
   return (
     <div
